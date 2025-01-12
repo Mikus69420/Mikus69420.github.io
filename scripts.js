378e1a82225f195c99eed5f2949df29f8de916a8
Mikus Baumanis <mikus.baumanis@gmail.com>
2025-01-12 14:09:54 +0200

    ok

--- scripts.js
+++ scripts.js
@@ -4,7 +4,24 @@ function mixColors() {
     const color3 = document.getElementById('color3').value;
 
     document.getElementById('result').style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
-    
+
 }
 
 let result = [];
+
+
+
+
+
+
+
+
+function genRandomColor() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    let hexColor = `#${randomColor}`;
+    let rgbColor = hexToRgb(hexColor);
+
+    document.getElementById('generateColor').style.backgroundColor = hexColor;
+    document.getElementById('colorHex').innerText = hexColor;
+    document.getElementById('colorRgb').innerText = `RGB(${rgbColor})`;
+}
