7ff486913e732cee1ab3f21b620e7588eb9e8753
Janis Baumanis <janis.baumanis@gmail.com>
2025-01-12 16:42:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ function mixColors() {
     const color3 = document.getElementById('color3').value;
 
     document.getElementById('result').style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
-
+     $("#resulttable").json2html(data,template);
 }
 
 let template = {'<>':'div','html':'${c1} ${c2} ${c3} ${res}'};
 
-let result = [
+let data = [
     {"c1":"23","c2":"34","c3":"128","res":"res1"},
     {"c1":"23","c2":"34","c3":"128","res":"res2"},
     {"c1":"23","c2":"34","c3":"128","res":"res3"},    
