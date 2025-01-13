571193f1ee19458f0b70c37c785cf14f034ed5eb
Mikus Baumanis <mikus.baumanis@gmail.com>
2025-01-13 20:23:45 +0200

    ok

--- scripts.js
+++ scripts.js
@@ -8,9 +8,9 @@ function mixColors() {
     const color1 = document.getElementById('color1').value;
     const color2 = document.getElementById('color2').value;
     const color3 = document.getElementById('color3').value;
-    let hexColor=  rgbToHex(color1,color2,color3);
+    let hexColor = rgbToHex(color1, color2, color3);
 
-    data.push({ "c1": color1 , "c2": color2 , "c3": color3, "res": hexColor });
+    data.push({ "c1": color1, "c2": color2, "c3": color3, "res": hexColor });
 
     document.getElementById('result').style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
     $('#resulttable tbody').empty();
@@ -22,7 +22,7 @@ function mixColors() {
 function rgbToHex(red, green, blue) {
     const rgb = (red << 16) | (green << 8) | (blue << 0);
     return '#' + (0x1000000 + rgb).toString(16).slice(1);
-  }
+}
 
 function clearColors() {
     data = [];
